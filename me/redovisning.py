22881758eef92a25c31c7a465e7b06619b6aaa65
Daniel V <daniel.vernberg@gmail.com>
2015-01-18 23:17:02 +0100

    redovisning.py
    made redovisning.py after redovisning.cgi

--- me/redovisning.py
+++ me/redovisning.py
@@ -2,5 +2,40 @@
 # -*- coding: utf-8 -*-
 
 """
-Update this.
+Mina redovisningar
 """
+
+print("""
+Min Redovisnings-sida
+==============================================================================
+
+
+Kmom01:
+------------------------------------------------------------------------------
+
+Allmänt
+Då var det första momentet avklarat. På det stora hela har det gått bra att installera de bitar av utvecklingsmiljön som krävs. Det gick även bra att jobba igenom guiderna för att skapa en me-sida och ett första python-skript.
+
+Vilken utvecklingsmiljö använder du?
+Jag använder mig av en MacBook Pro med OS X Yosemite. Som text editor använder jag Sublime Text. Har tidigare använt mig av Coda men hag mer och mer fastnat för Sublime Text. Gillar möjligheten att konfigurera Sublime Text som man vill och installera paket som gör editorn ännu kraftfullare. Är det något som ska göras i terminalen (vilket det hittils har varit rätt mycket av) har jag inför detta kursmoment installerat iTerm och tänkte se vad det går för. Har även GitHub for Mac installerad och gillar hellre att jobba med det verktyget än att använda terminalen för att sköta min repon. 
+Firefox, Chrome och Safari finns installerade. Använder egetligen mest Firefox i utvecklingssyfte tack vare firebug och dylikt men annars är det mest Safari som gäller till vanlig surfning.
+
+Är du bekant med Python sedan tidigare?
+Jag är inte bekant med python sedan tidigare utan det är något helt nytt för mig. Googlar jag lite rekommenderas det här och där att python är ett bra nybörjarspråk med en "enkel" syntax. Så förhoppningsvis kommer kursen gå bra då jag som sagt är helt nybörjare när det gäller python.
+
+Är du bekant med programmering och problemlösning sedan tidigare?
+Då jag har läst de andra kurspaketet som ges här på dbwebb har jag lite html, css , php och javascript i bagaget. Har även sedan tidigare läst en kurs i Visual Basic och en kurs i C++ på gymnasiet för ett par år sedan.
+
+Är du bekant med terminalen och Unix-kommandon sedan tidigare?
+Första gången jag kom i kontakt med terminalen och unix-kommandon var under en gymnasiekurs. Dock var det ingen speciel djupdykning utan mer simpla saker som att navigera i mappträd osv. Annars innehöll det en del inslag av terminalen och kommandon under det andra kurspaketet här på dbwebb. Men utöver det har jag inga mer erfarenheter av det.
+
+Gick det bra att komma i gång med kursmomentet, var det lagom, för litet, för stort?
+Det gick bra att komma igång med kursmomentet. Det var lite ovant, men smidigt, att lägga upp filer på studentservern direkt i terminalen. Annars har jag använt mig av antingen FileZilla eller Transmit för att göra det. Så det var ett nytt inslag. Det kändes lagom med uppgiften då det kan vara lite knepigt att få till utvecklingsmiljön och lite sådant som tar tid. Då jag sedan tidigare använt mig av Git och GitHub var det inga svårigheter att klona python-repot
+
+Vilken del av kursmaterialet (böcker, artiklar, videor, etc) uppskattade du mest?
+Jag tror nog att jag gillade alla videoklipp mest. Gillar nog att höra samt att se kodexemplen istället för att bara läsa i en bok. Böckerna var ändå bra, gillade framförallt python for informatics då det var relativt lättläst och på så vis blev det roligt att läsa igenom kapitlerna för momentet. Med allt kursmaterial ihop blev man väl förberedd för guiderna och uppgifterna.
+
+
+
+
+""")
